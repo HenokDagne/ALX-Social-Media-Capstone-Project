6f9ba28abd3de5386543d96a4acec92d614303c5
henok dagne <henokdagne19@gmail.com>
2025-08-12 19:26:49 +0300

    adding filed in customuser model

--- social_media_api/user/models.py
+++ social_media_api/user/models.py
@@ -1,8 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import RegexValidator
+
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    pass
+    phone_regex = RegexValidator(
+         regex=r'^\+?[1-9]\d{1,14}$',  # E.164 international format
+        message="Phone number must be entered in the format: '+<countrycode><number>' (up to 15 digits)."
+    )
+
+    phone_number = models.CharField(
+        validators=[phone_regex],
+        max_length=16,
+        unique=True,
+        blank=True,
+        null=True,
+    )
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 

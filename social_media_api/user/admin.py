0ebb7fd44c721b9e43d6cb0b0be48b701133a8cb
henok dagne <henokdagne19@gmail.com>
2025-08-13 10:20:38 +0300

    registering customuser model in admin site

--- social_media_api/user/admin.py
+++ social_media_api/user/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Optionally, customize fields displayed in admin
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone_number', 'bio', 'profile_picture')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('phone_number', 'bio', 'profile_picture')}),
+    )
+    list_display = UserAdmin.list_display + ('phone_number', 'bio', 'profile_picture')
+    search_fields = ('email', 'username', 'phone_number', 'bio')
+    ordering = ('email',)
+    list_editable = ('phone_number', 'bio', 'profile_picture')

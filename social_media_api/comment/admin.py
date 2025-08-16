5e0578c2e5ac4be5039ee815477a7b11cdd144cb
henok dagne <henokdagne19@gmail.com>
2025-08-16 20:50:30 +0300

    registry comment  model in admin

--- social_media_api/comment/admin.py
+++ social_media_api/comment/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from comment.models import Comment
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('id', 'post', 'user', 'content', 'created_at')
+	readonly_fields = ('created_at',)
+	search_fields = ('content', 'user__username')
+
+

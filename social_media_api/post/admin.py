b237270c95a7fdfcb331a412659e6bc1a57ae45a
henok dagne <henokdagne19@gmail.com>
2025-08-16 20:48:36 +0300

    registry Post model in admin

--- social_media_api/post/admin.py
+++ social_media_api/post/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'user', 'content', 'create_at', 'updated_at', 'image')
+	list_editable = ('title', 'content', 'image')
+	readonly_fields = ('create_at', 'updated_at')
+	search_fields = ('title', 'user__username')
